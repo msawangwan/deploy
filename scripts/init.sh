0c248e25514c1556e8411634692d515db9a8559f
msawangwan <misha@sawangwan.me>
2017-09-03 22:39:04 +0000

    expose the correct ports init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ..
-sudo docker build -t ci.io
-sudo docker run -it --rm --name ci.io.running ci.io
+sudo docker build -t ci.io .
+sudo docker run -p 9001:80 -it --rm --name ci.io.running ci.io
