041a3e47ad984da1c5fdcd0e1fa23bf6ff3e4af2
misha sawangwan <misha@sawangwan.me>
2017-09-04 10:56:27 -0700

    remove sudo

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd ..
-sudo docker build -t ci.io .
-sudo docker run -p 9001:80 -it --rm --name ci.io.running ci.io
+
+docker build -t ci.io .
+docker run -p 9001:80 -it --rm --name ci.io.running ci.io
 
 pwd
