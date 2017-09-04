96a0c0f010736b673da7dc4a2dcc645d43e84804
misha sawangwan <misha@sawangwan.me>
2017-09-04 10:42:24 -0700

    not able to execute bash script

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,3 +3,5 @@
 cd ..
 sudo docker build -t ci.io .
 sudo docker run -p 9001:80 -it --rm --name ci.io.running ci.io
+
+pwd
--- scripts/webhook.sh
+++ scripts/webhook.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "MY DIR"
+pwd
 echo "EXECUTE WEBHOOK.SH"
