a8f65cbe63b612d9c1ce7670297f9389ff1dde1e
misha sawangwan <misha@sawangwan.me>
2017-09-03 19:07:52 -0700

    need to print json data

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"html"
 	"log"
@@ -8,16 +10,36 @@ import (
 )
 
 const (
-	kENDPOINT = "/webhooks/payload"
-	kPORT     = ":80"
+	endpoint = "/webhooks/payload"
+	port     = ":80"
 )
 
 func main() {
-	log.Printf("listen on %s", kPORT)
+	log.Printf("listen on %s", port)
 
-	http.HandleFunc(kENDPOINT, func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
+
+		var data struct {
+			String string
+		}
+
+		decoder := json.NewDecoder(r.Body)
+
+		err := decoder.Decode(&data)
+
+		if err != nil {
+			log.Printf("err %s", err)
+		}
+
+		fmt.Println(data.String)
 	})
 
-	log.Fatal(http.ListenAndServe(kPORT, nil))
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func prettyPrintJSON(b []byte) ([]byte, error) {
+	var out bytes.Buffer
+	err := json.Indent(&out, b, "", "\t")
+	return out.Bytes(), err
 }
