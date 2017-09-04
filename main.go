d02e5b4a6d1a95953eea4038934c60c1d690cc62
misha sawangwan <misha@sawangwan.me>
2017-09-04 12:12:34 -0700

    use sh instead of bash

--- main.go
+++ main.go
@@ -64,7 +64,7 @@ func main() {
 		// - pulls the latest code from a repository
 		// - makes a build
 
-		cmd := exec.Command("/bin/bash", "./webhooks.sh")
+		cmd := exec.Command("/bin/sh", "./webhooks.sh")
 		err = cmd.Run()
 
 		if err != nil {
