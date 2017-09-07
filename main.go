8303b7f87f911e4e5cf2cc79913f88b7f61b077a
misha sawangwan <misha.sawangwan@gmail.com>
2017-09-07 16:33:06 -0700

    get the env from main.go

--- main.go
+++ main.go
@@ -3,14 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 const (
-	endpoint = "/webhooks/payload"
-	port     = ":80"
+	version    = "1.30"
+	endpoint   = "/webhooks/payload"
+	controller = "CIIO_ROOT_IPADDR"
+	port       = ":80"
+)
+
+var (
+	dockerHostAddr string
 )
 
 func main() {
-	http.HandleFunc(endpoint, server.HandleWebhook)
+	dockerHostAddr = os.Getenv(controller)
+
+	log.Printf("docker host addr, from env var: %s", dockerHostAddr)
+
+	http.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
+		// resp, err := client.Get("")
+	})
+
 	log.Fatal(http.ListenAndServe(port, nil))
 }
