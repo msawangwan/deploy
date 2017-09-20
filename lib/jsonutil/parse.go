49af1d319d4c3b3498f96172ed1bfad10522018c
misha sawangwan <misha.sawangwan@gmail.com>
2017-09-20 15:41:09 -0700

    cleaned up doc strings for the parse package

--- lib/jsonutil/parse.go
+++ lib/jsonutil/parse.go
@@ -8,12 +8,12 @@ import (
 	"os"
 )
 
-// FromReader Decodes json in a stream format
+// FromReader is a helper function that decodes a reader (assumed to be valid JSON) into a struct of the same object type
 func FromReader(r io.Reader, v interface{}) error {
 	return json.NewDecoder(r).Decode(v)
 }
 
-// FromFilepath is a wrapper for FromReader
+// FromFilepath wraps FromReader, making it easy to read JSON files given a filepath string
 func FromFilepath(p string, v interface{}) error {
 	r, e := os.Open(p)
 	if e != nil {
@@ -23,7 +23,7 @@ func FromFilepath(p string, v interface{}) error {
 	return FromReader(r, v)
 }
 
-// BufPretty formats json into a buffer
+// BufPretty pretty-print formats a reader into a bytes.Buffer, assumes the reader is JSON
 func BufPretty(r io.Reader, delim, indent string) (out bytes.Buffer, e error) {
 	src, e := ioutil.ReadAll(r)
 	if e != nil {
