4319785bc485c32b748206d609967ef41e8eb3a7
misha sawangwan <misha.sawangwan@gmail.com>
2017-09-19 17:39:15 -0700

    breakup the template strings onto multiple lines then trim whitespace

--- lib/dock/command.go
+++ lib/dock/command.go
@@ -15,7 +15,14 @@ type ContainerCommand struct {
 
 // Build satisfies the APIEndpointResolver interface
 func (c ContainerCommand) Build() string {
-	return `{{with $c := .URLComponents}}{{$c.Command}}/{{$c.Option}}{{if $c.Parameters}}?{{range $k, $v := $c.Parameters}}{{$k}}={{$v}}&{{end}}{{end}}{{end}}`
+	return `{{- with $c := .URLComponents -}}
+				{{- $c.Command -}}/{{- $c.Option -}}
+				{{- if $c.Parameters -}}
+					?{{- range $k, $v := $c.Parameters -}}
+						{{- $k -}}={{- $v -}}&
+					{{- end -}}
+				{{- end -}}
+			{{- end -}}`
 }
 
 // ContainerCommandByID represents any command given to a unique docker container of the form:
@@ -27,5 +34,12 @@ type ContainerCommandByID struct {
 
 // Build satisfies the APIEndpointResolver interface
 func (c ContainerCommandByID) Build() string {
-	return `{{with .}}{{.URLComponents.Command}}/{{.ID}}/{{.URLComponents.Option}}{{if .URLComponents.Parameters}}?{{range $k, $v := .URLComponents.Parameters}}{{$k}}={{$v}}&{{end}}{{end}}{{end}}`
+	return `{{- with . -}}
+				{{- .URLComponents.Command -}}/{{- .ID -}}/{{- .URLComponents.Option -}}
+				{{- if .URLComponents.Parameters -}}
+					?{{- range $k, $v := .URLComponents.Parameters -}}
+						{{- $k -}}={{- $v -}}&
+					{{- end -}}
+				{{- end -}}
+			{{- end -}}`
 }
