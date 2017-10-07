56e82742db7d4681731849930898fee480c65cab
misha sawangwan <misha.sawangwan@gmail.com>
2017-10-06 18:10:09 -0700

    fixed broken unit tests

--- lib/dir/workspace.go
+++ lib/dir/workspace.go
@@ -2,18 +2,9 @@ package dir
 
 import (
 	"io/ioutil"
+	"os"
 	"sync"
 )
-
-// TempDirDoesNotExistError ...
-// type TempDirDoesNotExistError struct {
-// 	DirPrefix string
-// }
-
-// // Error ...
-// func (e TempDirDoesNotExistError) Error() string {
-// 	return fmt.Sprintf("err: temp directory [%s] does not exist", e.DirPrefix)
-// }
 
 // WorkspaceCache ...
 type WorkspaceCache struct {
@@ -47,10 +38,17 @@ func (wc *WorkspaceCache) Fetch(k string) string {
 
 // Flush ...
 func (wc *WorkspaceCache) Flush() (n int, e error) {
+	for _, v := range wc.store {
+		er := os.Remove(v)
+		if er != nil {
+			return
+		}
+		n++
+	}
 	return
 }
 
-// NewWorkspaceCachee ...
+// NewWorkspaceCache ...
 func NewWorkspaceCache() *WorkspaceCache {
 	return &WorkspaceCache{
 		store: map[string]string{},
@@ -66,72 +64,3 @@ func MkTempWorkspace(prefix string) (d string, e error) {
 
 	return
 }
-
-/* DEPRECATED BELOW THIS COMMENT */
-
-// // WorkspaceCacher ...
-// type WorkspaceCacher interface {
-// 	MkTempDir(prefix string) (d string, er error)
-// 	FindTempDir(prefix string) (d string, er error)
-// 	FlushAll() (flushcount int, er error)
-// }
-
-// NewWorkspaceCache ...
-// func NewWorkspaceCache() WorkspaceCacher {
-// 	return &WorkspaceTable{
-// 		cache: map[string]string{},
-// 	}
-// }
-
-// WorkspaceTable ...
-// type WorkspaceTable struct {
-// 	cache map[string]string
-// 	sync.Mutex
-// }
-
-// // MkTempDir ...
-// func (dt *WorkspaceTable) MkTempDir(prefix string) (d string, er error) {
-// 	dir, er = ioutil.TempDir("./", prefix)
-// 	if er != nil {
-// 		return
-// 	}
-
-// 	dt.Lock()
-// 	defer dt.Unlock()
-// 	{
-// 		dt.cache[prefix] = dir
-// 	}
-
-// 	return
-// }
-
-// // FindTempDir ...
-// func (dt *WorkspaceTable) FindTempDir(prefix string) (d string, er error) {
-// 	dt.Lock()
-// 	defer dt.Unlock()
-// 	{
-// 		if cached, found := dt.cache[prefix]; found {
-// 			d = cached
-// 		} else {
-// 			er = TempDirDoesNotExistError{prefix}
-// 		}
-// 	}
-
-// 	return
-// }
-
-// // FlushAll ...
-// func (dt *WorkspaceTable) FlushAll() (flushcount int, er error) {
-// 	dt.Lock()
-// 	defer dt.Unlock()
-// 	{
-// 		if len(dt.cache) > 0 {
-// 			for _, d := range dt.cache {
-// 				os.Remove(d)
-// 				flushcount++
-// 			}
-// 		}
-// 	}
-
-// 	return
-// }
--- lib/dir/workspace_test.go
+++ lib/dir/workspace_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestBasicWorkspaceCacherImpl(t *testing.T) {
-	var dc WorkspaceCacher
+	var dc *WorkspaceCache
 
 	dc = NewWorkspaceCache()
 
@@ -17,39 +17,41 @@ func TestBasicWorkspaceCacherImpl(t *testing.T) {
 
 	pre := "test_tmp_dir"
 
-	dir, er := dc.MkTempDir(pre)
+	dir, er := MkTempWorkspace(pre)
 	if er != nil {
 		t.Fatalf("%s %s", er, symbol.FailMark)
 	}
 
 	defer os.RemoveAll(dir)
 
+	dc.Store(pre, pre)
+
 	t.Logf("created and cached dir: %s %s", dir, symbol.PassMark)
 
-	dir, er = dc.FindTempDir(pre)
-	if er != nil {
-		t.Fatalf("%s %s", er, symbol.FailMark)
-	}
+	dir = dc.Fetch(pre)
 
 	t.Logf("fetched dir: %s %s", dir, symbol.PassMark)
 }
 
 func TestFlushingTheCache(t *testing.T) {
-	var dc WorkspaceCacher
+	var dc *WorkspaceCache
 
 	dc = NewWorkspaceCache()
 
 	t.Logf("created a directory cacher: %s", symbol.PassMark)
 
 	for i := 0; i < 20; i++ {
-		dir, er := dc.MkTempDir(fmt.Sprintf("test_prefix_%d", i))
+		dir, er := MkTempWorkspace(fmt.Sprintf("test_prefix_%d", i))
 		if er != nil {
 			t.Fatalf("%s", er)
 		}
+
+		dc.Store(string(i), dir)
+
 		t.Logf("created a tmpdir: %s", dir)
 	}
 
-	cleared, er := dc.FlushAll()
+	cleared, er := dc.Flush()
 	if er != nil {
 		t.Fatalf("%s", er)
 	}
